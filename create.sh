f6bddbef0920d70f6da7f2f4a49c23efcec5350b
bnewbold <bnewbold@robocracy.org>
2011-07-21 18:59:32 -0400

    tweaks

--- create.sh
+++ create.sh
@@ -2,8 +2,6 @@
 
 curl -X DELETE "http://localhost:9200/oldpapers"
 
-#while read line; do echo $line | json _id; done < "data.json"
-#while read line; do curl -X POST "http://localhost:9200/oldpapers/paper/`echo $line | json _id`" -d '`echo $line`'; done < "data.json"
-while read line; do curl -X POST "http://localhost:9200/oldpapers/paper" -d "$line"; done < "data.json"
+while read line; do curl -X POST "http://localhost:9200/oldpapers/paper" -d "$line" &> /dev/null ; done < "data.json"
 
 curl -X POST "http://localhost:9200/oldpapers/_refresh"
